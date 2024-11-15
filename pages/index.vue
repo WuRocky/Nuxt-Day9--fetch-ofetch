1dba2399f01f56e8c52d7c6fab57a1e49227b57c
Rocky Wu <rocky_wu@everrich.com.tw>
2024-11-15 12:15:44 +0800

    day9-$fetch&ofetch

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,47 @@
 <script setup>
+// // 改法一.用 useFetch
+// const { data } = await useFetch("https://randomuser.me/api/");
+// const [result] = data.value.results;
+
+// // 改法二.用 useFetch 搭配 $fetch()
+// const { data } = await useAsyncData("getRandomuser", () =>
+//   $fetch("https://randomuser.me/api/")
+// );
+
+
+// // 適合使用 $fetch 的情境
+// const userData = ref({});
+
+// const getUserData = async () => {
+//   const response = await $fetch("https://randomuser.me/api/");
+//   userData.value = response.results[0];
+// };
+
+
 navigateTo("/register");
 </script>
 
 <template>
+  <!-- <div>Page: index</div>
+
+  <img
+    :src="result.picture?.large"
+    :alt="`${result.name.first} ${result.name?.last}`"
+  />
+  <h1>{{ result.name?.first }} {{ result.name?.last }}</h1>
+  <h3>Email: {{ result.email }}</h3>
+
+  <template v-if="userData.name">
+    <img
+      :src="userData.picture?.large"
+      :alt="`${userData.name?.first} ${userData.name?.last}`"
+    />
+    <h1>{{ userData.name?.first }} {{ userData.name?.last }}</h1>
+    <h3>Email: {{ userData.email }}</h3>
+  </template>
+
+  <button type="button" @click="getUserData">取得 User 資料</button> -->
+
+
   <div>Page: index</div>
 </template>
-
-<style scoped></style>
--- pages/register.vue
+++ pages/register.vue
@@ -13,6 +13,64 @@ const userRegisteObject = ref({
     detail: "",
   },
 });
+
+const sendContact = async () => {
+  if (!userRegisteObject.value.name || !userRegisteObject.value.email || !userRegisteObject.value.password || !userRegisteObject.value.phone || !userRegisteObject.value.birthday || !userRegisteObject.value.address.zipcode || !userRegisteObject.value.address.detail) {
+      $swal.fire({
+        icon: "error",
+        title: "所有欄位皆為必填",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      return;
+    }
+
+  const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  if (!passwordPattern.test(userRegisteObject.value.password)) {
+    $swal.fire({
+      icon: "error",
+      title: "密碼需至少包含 8 碼，並英數混合",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    return;
+  }
+
+  const phonePattern = /^((\+886|0)?9\d{8}|(\+886|0)?\d{1,2}-\d{6,8})$/;
+  if (!phonePattern.test(userRegisteObject.value.phone)) {
+    $swal.fire({
+      icon: "error",
+      title: "電話格式錯誤",
+      text: "請輸入正確的手機號碼或市內電話",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    return;
+  }
+
+
+  try {
+    const response = await $fetch("https://todolist-api.hexschool.io/doc/#/%E4%BD%BF%E7%94%A8%E8%80%85/post_users_sign_up", {
+      method: "POST",
+      body: { ...userRegisteObject.value }, // 傳遞物件的值
+    });
+    $swal.fire({
+      icon: "success",
+      title: "恭喜您註冊成功",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  } catch (error) {
+    $swal.fire({
+      icon: "error",
+      title: "註冊失敗",
+      text: error.response?.data?.message || "請稍後再試",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  }
+};
+
 // 使用 sweetAlert2 套件顯示訊息
 // $swal.fire({
 //   position: "center",
@@ -30,13 +88,14 @@ const userRegisteObject = ref({
         <div class="col-12 col-md-11 col-lg-8 col-xl-7 col-xxl-6">
           <div class="bg-white p-4 p-md-5 rounded shadow-sm">
             <h2 class="h3 mb-4">會員註冊</h2>
-            <form @submit.prevent="">
+            <form @submit.prevent="sendContact">
               <div class="form-floating mb-4">
                 <input
                   type="text"
                   class="form-control"
                   id="firstName"
                   placeholder="王小明"
+                  v-model="userRegisteObject.name"
                   required
                 />
                 <label for="firstName"
@@ -50,7 +109,8 @@ const userRegisteObject = ref({
                   class="form-control"
                   id="email"
                   placeholder="example@gmail.com"
-                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                  pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+                  v-model="userRegisteObject.email"
                   required
                 />
                 <label for="email"
@@ -63,8 +123,9 @@ const userRegisteObject = ref({
                   type="password"
                   class="form-control"
                   id="password"
-                  placeholder="請輸入 8 碼以上密碼"
-                  pattern=".{8,}"
+                  placeholder="請輸入 8 碼以上密碼，需包含英數"
+                  pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+                  v-model="userRegisteObject.password"
                   required
                 />
                 <label for="password"
@@ -77,8 +138,9 @@ const userRegisteObject = ref({
                   type="tel"
                   class="form-control"
                   id="phone"
-                  placeholder="0912345678"
-                  pattern="(\+886|0)?9\d{8}|(\+886|0)?2\d{8}|\d{3}-\d{4}-\d{4}"
+                  placeholder="0912345678 或 02-12345678"
+                  pattern="^((\+886|0)?9\d{8}|(\+886|0)?\d{1,2}-\d{6,8})$"
+                  v-model="userRegisteObject.phone"
                   required
                 />
                 <label for="phone">電話</label>
@@ -89,6 +151,7 @@ const userRegisteObject = ref({
                   type="date"
                   class="form-control"
                   id="dateInput"
+                  v-model="userRegisteObject.birthday"
                   required
                 />
                 <label for="dateInput">出生年月日</label>
@@ -103,6 +166,7 @@ const userRegisteObject = ref({
                       id="zipcode"
                       placeholder="100"
                       pattern="\d{3,}"
+                      v-model="userRegisteObject.address.zipcode"
                       required
                     />
                     <label for="zipcode">郵遞區號</label>
@@ -115,6 +179,7 @@ const userRegisteObject = ref({
                       class="form-control"
                       id="address"
                       placeholder="台北市中正區重慶南路一段"
+                      v-model="userRegisteObject.address.detail"
                       required
                     />
                     <label for="address">詳細地址</label>
